552ae378d40ff7b1dfa305a64cfe0d98606dddc7
Carl <carl.mcallansmith@gmail.com>
2017-05-08 22:27:47 +0000

    More wheel movements with motor scaling

--- avc1.cpp
+++ avc1.cpp
@@ -8,13 +8,15 @@ int main()
   int count = 0;
   char pix[32];
   char wh[32];
-  int err = 0;
+  double err = 0.0;
   int nwp =0 ;
+  int v_left = 0;
+  int v_right = 0;
   
   while(count < 8000)
 	{
 		take_picture();
-		display_picture(1,0);
+		display_picture(0,0);
 		// get pixel values and  decide if pixels are white
 		for ( int i = 0 ; i < 32 ;i++)
 		{
@@ -27,7 +29,7 @@ int main()
 		}
 		// calculate how far line is from the centre and
 		// how many white pixel are there
-		err = 0;
+		err = 0.0;
 		nwp = 0;
 		for ( int i = 0 ; i < 32 ;i++)
 		{
@@ -37,18 +39,23 @@ int main()
 				nwp = nwp + 1;
 			}
         }
+        err = err/((double)nwp);
+        double scale = 1.0;
+        v_left = 60 + (int)(err*scale);
+        v_right = 60 - (int)(err*scale);
+        set_motor(1,v_left);
+        set_motor(2,v_right);
+        
 		// print all arrays
-		for ( int i = 0 ; i < 32 ;i++)
-		{
-			printf("%d ",pix[i]);
-        }		
-		printf("\n");
+		//for ( int i = 0 ; i < 32 ;i++)
+		//{printf("%d ",pix[i]);   }		
+		//printf("\n");
 		for ( int i = 0 ; i < 32 ;i++)
 		{
 			printf("%d ",wh[i]);
         }		
 		printf("\n");
-		printf("err=%d nwp=%d\n",err,nwp);
+		printf("err=%f nwp=%d v_left=%d v_righ=%d\n",err,nwp,v_left,v_right);
 		
 		sleep1(1,0);
 		count++;
@@ -58,7 +65,7 @@ int main()
     }
     
    // close_screen_stream();
-	printf("After init() error=%d\n",err);
+	printf("After init() error=%f\n",err);
 	printf("Again\n");
 	
 	return 0;
