a4e858ced31e0da1306c1fbe99d3288aaaa52533
Carl <carl.mcallansmith@gmail.com>
2017-05-18 05:36:14 +0000

    new main file

--- clapmain.cpp
+++ clapmain.cpp
@@ -6,5 +6,25 @@ int DEBUG =1;
 
 int main()
 {
-  printf(" Hello\n");
+  printf(" Hello I'll prentend to work \n");
   init();
+  
+	int count = 0;
+	int pix[320];   //set pixel array to 32 wide
+	int wh[320];    //for white pixel array
+	double err = 0.0;
+	double nwp =0 ; //number of white pixels
+	double scale = 2.0;
+	double v_left = 0; // left motor
+	double v_right = 0;  // right motor
+	float kp = 0.5; //This is the PID, 'Proportionality'
+	int proportional_signal =0;
+	
+	while(count < 8000)
+	{
+		take_picture();
+		display_picture(1,0);
+		
+	}
+	
+}
